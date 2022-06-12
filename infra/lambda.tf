521e5d8fa7d98e407fde44f19a2c5666d0392698
Sebastian <seb@ruse.tech>
2022-06-11 23:13:31 -0700

    update lambda

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -4,8 +4,12 @@ module "jita_modifiy_iam" {
   function_name = "jita_modifiy_iam"
   description   = "Modified jita role to allow user access"
   handler       = "index.lambda_handler"
-  runtime       = "python3.8"
-
+  runtime       = "python3.9"
+  lambda_role = aws_iam_role.lambda_jita_modifiy_iam_role.arn
   source_path = "src/index.py"
+
+  environment_variables = {
+    ADMIN_ROLE_NAME = aws_iam_role.jita_admin.name
+  }
 
 }
