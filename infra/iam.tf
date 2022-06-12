a5c1e5280b0332d511d8a3b2df1829f36a65c7a8
Sebastian <seb@ruse.tech>
2022-06-11 23:13:20 -0700

    add iam

---
+++ infra/iam.tf
@@ -0,0 +1,62 @@
+data "aws_iam_policy_document" "lambda_modifiy_jita_admin_role" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "iam:GetRole",
+      "iam:UpdateAssumeRolePolicy",
+    ]
+
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+
+}
+
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = [ "lambda.amazonaws.com" ]
+    }
+  }
+}
+
+resource "aws_iam_policy" "lambda_modifiy_jita_admin_role" {
+  name = "lambda_modifiy_jita_admin_role"
+  description = "Allows modificaiton of jita-admin trust policy."
+  policy = data.aws_iam_policy_document.lambda_modifiy_jita_admin_role.json
+}
+
+resource "aws_iam_role" "lambda_jita_modifiy_iam_role" {
+  name               = "lambda_jita_modifiy_iam"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "attached_lambda_policy" {
+  role = aws_iam_role.lambda_jita_modifiy_iam_role.name
+  policy_arn = aws_iam_policy.lambda_modifiy_jita_admin_role.arn
+}
+
+resource "aws_iam_role" "jita_admin" {
+  name = "jita-admin"
+  assume_role_policy = data.aws_iam_policy_document.jita_admin_assume_role.json
+}
+
+data "aws_iam_policy_document" "jita_admin_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = [ "" ]
+    }
+    condition {
+      test = "StringLike"
+      variable = "sts:RoleSessionName"
+
+      values = ["$${aws:username}"]
+    }
+  }
+}
