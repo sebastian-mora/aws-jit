d85a252c222feaf0da807e82bfbe24d30605ae69
Sebastian <seb@ruse.tech>
2022-06-18 20:18:51 -0700

    add request timeout sqs

--- infra/sqs.tf
+++ infra/sqs.tf
@@ -1,5 +1,6 @@
 resource "aws_sqs_queue" "jita_request_queue" {
   name                      = "jita-request-queue"
+  message_retention_seconds = var.jita_request_timout
 }
 
 resource "aws_sqs_queue" "jita_message_queue" {
--- infra/vars.tf
+++ infra/vars.tf
@@ -1,3 +1,8 @@
 variable "jita_admin_policy_arn" {
   default = ""
 }
+
+variable "jita_request_timout" {
+  type = number
+  default = 3600 #seconds
+}
