43b9b97122ba0315e1e848d9848a6e017d8a3353
seb <seb@ruse.tech>
2022-06-19 09:17:10 -0700

    TFE Workspace
    Adding tfe workspace to enable auto deployment via Master branch.

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "ruse"
+
+    workspaces {
+      name = "aws-jita"
+    }
+  }
+}
+
 provider "aws" {
   default_tags {
     tags = {
