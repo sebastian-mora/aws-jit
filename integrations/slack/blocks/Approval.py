e1026161559fa411a6e89c17d693bc79e80427f2
Sebastian <seb@ruse.tech>
2022-06-18 20:12:53 -0700

    updating message formats

--- integrations/slack/blocks/Approval.py
+++ integrations/slack/blocks/Approval.py
@@ -2,50 +2,78 @@
 def create_approval_message(body, message_id):
 
   return [
+    	{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": "Just In Time Access Request",
+				"emoji": True
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": ":warning: Approving this message will grant *temporary* access for the *requested arn* to the jita-admin role :warning:"
+			}
+		},
         {
-                    "type": "section",
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": f"* *Requester*: @{body['requester_name']} \n * *Requested Arn*: {body['requester_arn']} \n * *Reason*: {body['reason']}"
+			}
+		},
+        {
+			"type": "context",
+			"elements": [
+				{
+					"type": "plain_text",
+					"text": "Default access time is set to 2hrs. Request expires in 1 hour.",
+					"emoji": True
+				}
+			]
+		},
+        {
+			"type": "divider"
+		},
+        {
+            "type": "actions",
+            
+            "elements": [
+                {
+                    "type": "button",
+                    "action_id":"request_approved",
                     "text": {
-                        "type": "mrkdwn",
-                        "text": f"@{body['requester_name']} is requesting JITA"
-                    }
+                        "type": "plain_text",
+                        "emoji": True,
+                        "text": "Approve"
+                    },
+                    "style": "primary",
+                    "value": "True"
                 },
                 {
-                    "type": "actions",
-                    
-                    "elements": [
-                        {
-                            "type": "button",
-                            "action_id":"request_approved",
-                            "text": {
-                                "type": "plain_text",
-                                "emoji": True,
-                                "text": "Approve"
-                            },
-                            "style": "primary",
-                            "value": "True"
-                        },
-                        {
-                            "type": "button",
-                            "action_id":"request_denied",
-                            "text": {
-                                "type": "plain_text",
-                                "emoji": True,
-                                "text": "Deny"
-                            },
-                            "style": "danger",
-                            "value": "False"
-                        }, 
-                    ]
-                },
+                    "type": "button",
+                    "action_id":"request_denied",
+                    "text": {
+                        "type": "plain_text",
+                        "emoji": True,
+                        "text": "Deny"
+                    },
+                    "style": "danger",
+                    "value": "False"
+                }, 
+            ]
+        },
+        {
+            "type": "context",
+            "block_id":"sqs_id",
+            "elements": [
                 {
-                    "type": "context",
-                    "block_id":"sqs_id",
-                    "elements": [
-                        {
-                            "type": "plain_text",
-                            "text": message_id,
-                            "emoji": True
-                        }
-                    ]
+                    "type": "plain_text",
+                    "text": message_id,
+                    "emoji": True
                 }
-                ]
+            ]
+        }
+    ]
